5771a11722179d8254eabf3a9ba89a7d85f12496
DaisyBrumit <dfrybrum@uncc.edu>
2023-06-14 20:03:19 -0400

    wrangle data

--- pc_accuracy.R
+++ pc_accuracy.R
@@ -7,24 +7,67 @@ library(naniar)
 library(reshape2)
 
 # Set global vars
-#beta_div <- c('bray_curtis', 'dada2', 'jaccard', 'phylo_rpca', 'phylo_ctf', 'rpca',
-              #'ctf', 'unweighted_unifrac', 'weighted_unifrac')
+beta.div <- c('bray_curtis', 'dada2', 'jaccard', 'phylo_rpca', 'rpca',
+              'unweighted_unifrac', 'weighted_unifrac')
 
-rootDir <- '/Users/dfrybrum/beta_diversity_testing/' # path to working dir
-study <- 'Vangay' # all studies (also Directories!)
-metric <- 'Accuracy' # all performace metrics
+rootDir <- '/Users/dfrybrum/git/beta_diversity_testing/' # path to working dir
+study.list <- c('Vangay')
+#study.list <- c('Vangay', 'Jones', 'Zeller', 'Noguera-Julian') # all studies (also Directories!)
+metric <- 'Accuracy' # want to track accuracy performance over time
 
-# set function for importing custom named dataframes
-df_import <- function(metric_string, nPC) {
-  #df <- read_tsv(paste0(study,'/',metric_string,'_table.txt'))
-  df_name <- paste0(study,nPC)
-  df_in <- read_tsv(paste0(study,'/',metric_string,'_table_',nPC,'.txt'))
-  
-  df_out <- assign(df_name, df)
-  return(df_out)
-}
+# create empty dfs
+df.vector <- list() # this empty list will hold all dataframes so I don't have to figure out how to call a df by name in a loop
 
-for (i in 3:10)
+for (beta in beta.div) # one dataframe per beta diversity approach
 {
-  df_import(str_to_lower(metric), i)
+  empty.df <- data_frame() # make an empty dataFrame
+  df.vector <- append(df.vector, list(empty.df)) # add it to the list
 }
+
+# read in dataframes
+for (i in 3:10) # for every PC count available
+{
+  list.index <- 1 # index through the list of dataframes, ONE per beta diversity metric
+  for (beta in beta.div) # for every metric
+  {
+    for (study in study.list) # read in every study associated with this metric executed on this many PCs 
+    {
+      df <- read_tsv(paste0(study,'/',metric,'_table_',i,'.txt')) # read in the table
+      df['pc_count'] <- paste0(i) # create a variable for PC count
+      df['study'] <- paste0(study) # create a variable for study (not used. Sanity check)
+      
+      df.tmp <- filter(df, method == beta) # filter out only observations with desired diversity metric
+      df.vector[[list.index]] <- bind_rows(df.vector[list.index], df.tmp) # append the subsetted values to the final dataframe of interest
+    }
+    list.index <- list.index + 1 # move into the next dataframe (and the next beta diversity metric) on the list
+  }
+}
+
+# create a plot for each beta diversity metric
+metaDF <- data_frame()
+for (item in df.vector)
+{
+  df <- item
+  #for (column in df) 
+  #{
+    #df <- replace_with_na_all(df, condition = ~.x == 999)
+  #}
+  
+  #feature_names <- colnames(df)[-which(colnames(df) == 'method')]
+  
+  df <- select(df, !one_of(c('study')))
+  feature_names <- select(df, !one_of(c('pc_count', 'method')))
+  feature_names <- colnames(feature_names)
+  
+  # average performance metric values per feature
+  feature_avgs <- df %>% group_by(pc_count, method) %>% summarise(across(everything(), mean, na.rm=TRUE))
+  
+  # expand averages into long form with 3 columns: method, feature name, and average value
+  feature_avgs <-feature_avgs %>% mutate(id = row_number()) %>% 
+    pivot_longer(cols = !contains('pc_count') & !contains('method'), names_to = "names", values_to = "values") %>%
+    filter(names != 'id')
+  
+  metaDF <- bind_rows(metaDF, feature_avgs)
+}
+
+plot <- multi_feature_boxplot(metaDF, '', metric)
