ee4801c3ff5edbdc79e5918fcb920c61d581495e
DaisyBrumit <dfrybrum@uncc.edu>
2023-08-30 14:51:03 -0400

    create script lib

--- scripts/plotting_functions.R
+++ scripts/plotting_functions.R
@@ -1,12 +1,35 @@
 # LIBRARY OF PLOTTING FUNCTIONS FOR BETA DIVERSITY STUDIES 
 # AUTHORED BY DAISY FRY BRUMIT
 
-model_metrics_boxplot <- function(x, y, model="Model", stat="p-value", subtitle=NULL)
+make.boxplot <- function(df, x, y, title=NULL, subtitle=NULL)
 { 
-  plt <- ggplot(df, aes(x=x, y=y)) +
+  # boxplot figure where
+  # df: dataframe containing other parameters
+  # x: metric of interest (accuracy)
+  # y: classes of interest (beta diversity method)
+  
+  plt <- ggplot(df, aes(x=x, y=y, color=group, group=group)) +
     geom_boxplot() +
-    labs(title = paste(model,stat,'per Method'), 
-         subtitle = subtitle) +
+    labs(title = title, subtitle = subtitle) +
+    theme(axis.text.x = element_text(angle=45, hjust=0.5, vjust=0.5), 
+          plot.title = element_text(size=10, face = 'bold'))
+  
+  return(plt)
+}
+
+make.linegraph <- function(df, x, y, group=NULL, scale=NULL, title=NULL, subtitle=NULL)
+{
+  # line graph where
+  # df: dataframe containing other parameters
+  # x: metric of interest (accuracy)
+  # y: derived trend to observe (# of principle components)
+  # group: classes of interest (beta diversity method)
+  # scale: a vector containing min and max Y-axis values
+  
+  plt <- ggplot(df, aes(x=x, y=y, color=group, group=group)) +
+    geom_smooth(na.rm=TRUE, se=FALSE) +
+    scale_y_continuous(limits=scale) +
+    labs(title=title, subtitle=subtitle) +
     theme(axis.text.x = element_text(angle=45, hjust=0.5, vjust=0.5), 
           plot.title = element_text(size=10, face = 'bold'))
   
