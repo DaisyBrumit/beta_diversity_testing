dd3f939b18e3b2549ec6df9ecfe2b1bf2a5ac16b
DaisyBrumit <dfrybrum@uncc.edu>
2024-01-22 12:29:52 -0500

    remove HA refs, reference new support scripts, handle gemelli data separately

--- scripts/3.3_plot_pcoa.R
+++ scripts/3.3_plot_pcoa.R
@@ -4,7 +4,8 @@ rm(list = ls())
 library(vegan)
 library(tidyverse)
 source('~/beta_diversity_testing/scripts/functions/meta_from_files.R')
-
+source('~/beta_diversity_testing/scripts/functions/dec_to_percent.R')
+source('~/beta_diversity_testing/scripts/functions/parse_fromBiplot.R')
 
 studyList <- c('Zeller', 'Jones', 'Vangay', 'Noguera-Julian', 'gemelli_ECAM')
 
@@ -18,58 +19,65 @@ for (study in studyList) {
   
   ### METADATA HANDLING: ONLY ONCE PER STUDY
   # read in metadata
-  meta <- read.csv(paste0('~/beta_diversity_testing/',study,'/refiltered_meta.txt'), 
+  meta <- read.csv(paste0('~/beta_diversity_testing/',study,'/meta.txt'), 
                      sep = '\t', row.names = 1, header = TRUE, check.names = FALSE)
-  char_cols <- names(meta)[sapply(meta, function(col) is.character(col) || is.factor(col))]
+  char_cols <- meta %>% select(where(is.factor) | where(is.character)) %>%
+    select(where(function(x) n_distinct(x) <= 6))
+  
   
   # find all distance matrix files for this study
-  files <- list.files(pattern = 'ordinations.tsv', recursive = FALSE)
+  files <- list.files(pattern = 'ordinations', recursive = FALSE)
   # need to run pcoa and plot for each file
   for (file in files){ 
-    
     # using filename, save actual beta method as a string
     beta_method <- get_beta(file)
     ntaxa <- get_n_taxa(file)
     print(paste("Beta Method for", study, beta_method, ntaxa)) #sanity check
     
-    # read in data as a table
-    data <- read.table(file, header=TRUE, sep='\t')
-    
-    # row 1 contains proportion var explained
-    prop_exp <- data[1,]
+    if (beta_method %in% c('phylo_rpca', 'rpca')) {
+      data <- parse_ords(file)
+      
+      prop_exp <- read.table(file, header=FALSE, sep='\t', check.names = FALSE,
+                             skip = 4, nrows=1)
+    } else if (beta_method %in% c('phylo_ctf', 'ctf')){
+      next
+    } else {
+      # read in data as a table
+      data <- read.table(file, header=TRUE, sep='\t')
+      
+      # row 1 contains proportion var explained
+      prop_exp <- data[1,]
+      data <- data[-1,] # we don't need prop explained here anymore
+    }
     
     # isolate percent var explained and assign indices
     row.names(data) <- data[[1]] # assign dist matrix row names
-    data <- data[-1,] # we don't need prop explained here anymore
     data <- data[,-1] # don't need index values as a column, either
     
-    data <- log10(data+1)
-    
-    for (column in char_cols) {
+    for (column in colnames(char_cols)) {
       # drop all rows where the specified feature is missing data
       meta <- meta[!is.na(meta[column]), ] 
       
-      # meta and data must share id's 
       shared.indices <- intersect(rownames(meta), rownames(data))
-      data <- data %>% filter(rownames(data) %in% shared.indices)
       meta <- meta %>% filter(rownames(meta) %in% shared.indices)
+      data <- data %>% filter(rownames(data) %in% shared.indices)
       
       # Create the scatter plot
       plot.table <- data.frame(MDS1=data$PC1, 
                                MDS2=data$PC2,
                                row.names=row.names(meta))
-      plot.table[[column]] <- as.factor(meta[[column]])
-      x.lab <- paste("MDS1:",as.character(round(prop_exp[2], digits=4))) # print first 4 decimals
-      y.lab <- paste("MDS2:",as.character(round(prop_exp[3], digits=4))) # print first 4 decimals
+      plot.table[[column]] <- as.factor(meta[[column]]) 
+      MDS1_pct <- paste('MDS1:', dec_to_percent(prop_exp[2])) # percent label
+      MDS2_pct <- paste('MDS2:', dec_to_percent(prop_exp[3])) # percent label
       
       plot <- ggplot(plot.table, aes(x=MDS1, y=MDS2, color=plot.table[[column]])) +
         geom_point(size=2) + 
         #stat_ellipse() +
-        labs(title = paste(study, beta_method, ntaxa, "log 10 PCOA RESULTS"), 
+        labs(title = paste(study, beta_method, ntaxa, "PCOA Results"), 
              subtitle = paste("meta column = ", column), 
-             fill=plot.table[[column]]) +
-        xlab(x.lab) + 
-        ylab(y.lab)
+             color= column) +
+        xlab(MDS1_pct) + 
+        ylab(MDS2_pct)
       
       print(plot) 
     }
