3129c87e92ff7d8f4fe646e76f28c2d4b2a00b2b
DaisyBrumit <dfrybrum@uncc.edu>
2024-01-23 13:11:06 -0500

    compare 2 different ord outputs, add plot by time component

--- scripts/3.5_plot_pcoa_ctf.R
+++ scripts/3.5_plot_pcoa_ctf.R
@@ -20,16 +20,16 @@ for (study in studyList) {
   if (study == "gemelli_ECAM") {
     feature <- 'delivery' # this is the feature we'll color code by
     index <- 'host_subject_id' # repeat subject id 
-    #group <- 'month' # feature associated with replicate measures
+    group <- 'month' # feature associated with replicate measures
     
   } else if (study == "Jones") {
     feature <- 'Genotype' # this is the feature we'll color code by
     index <- 'repeat_ID'  # repeat subject id
-    #group <- 'Visit' # feature associated with replicate measures
+    group <- 'Visit' # feature associated with replicate measures
   }
   
   # find all metadata/ordination files for this study
-  files <- list.files(pattern = 'fromBiplot.tsv', recursive = FALSE)
+  files <- list.files(pattern = 'ctf_ords', recursive = FALSE)
   
   # need to run pcoa and plot for each file
   for (file in files){ 
@@ -40,32 +40,59 @@ for (study in studyList) {
     print(paste("Beta Method for", study, beta_method, ntaxa)) #sanity check
     
     # proportion explained is in line 5 of the biplot file
-    prop_exp <- read.table(file, header=FALSE, sep='\t', check.names = FALSE,
+    prop_exp_file <- paste0(beta_method,'_ordinations_fromBiplot.tsv')
+    prop_exp <- read.table(prop_exp_file, header=FALSE, sep='\t', check.names = FALSE,
                            skip = 4, nrows=1)
-    
     # retrieve data
-    df <- parse_ords(file)
+    data.with.meta <- read.table(file, sep = '\t', check.names = FALSE, header = TRUE, 
+                     comment.char = '')
     
-    # read in metadata %>% select only relevant info %>% remove redundant rows
-    meta <- read.csv(paste0('~/beta_diversity_testing/',study,'/meta.txt'), 
-                     sep = '\t', header = TRUE, check.names = FALSE) %>%
-      select(index, feature) %>% distinct()
-    row.names(meta) <- meta[[index]]
-    
+    # GENERAL PLOT #
     # create labels
-    x.lab <- paste("MDS1:",as.character(round(prop_exp[2], digits=4))) # print first 4 decimals
-    y.lab <- paste("MDS2:",as.character(round(prop_exp[3], digits=4))) # print first 4 decimals
+    MDS1.lab <- paste("MDS1:",as.character(round(prop_exp[2], digits=4))) # print first 4 decimals
+    MDS2.lab <- paste("MDS2:",as.character(round(prop_exp[3], digits=4))) # print first 4 decimals
     
-    plot <- ggplot(df, aes(x=PC1, y=PC2, color=meta[[feature]])) +
+    PC_plot_sso <- ggplot(data.with.meta, aes(x=PC1, y=PC2, color=.data[[feature]])) +
       geom_point(size=2) + 
-      #stat_ellipse() +
       labs(title = paste(study, beta_method, ntaxa, "PCOA RESULTS"), 
            subtitle = paste("meta feature = ", feature), 
-           fill=df[[feature]]) +
-      xlab(x.lab) + 
-      ylab(y.lab)
+           color = feature) +
+      xlab(MDS1.lab) + 
+      ylab(MDS2.lab)
     
-    print(plot)
+    print(PC_plot_sso)
+    
+    # PLOT OVER GROUP #
+    PCGroup_plot_sso <- ggplot(data.with.meta, aes(x=.data[[group]], 
+                              y=PC1, color=.data[[feature]])) +
+      geom_point(size=2) + 
+      labs(title = paste(study, beta_method, ntaxa, "PCOA RESULTS"), 
+           subtitle = paste("meta feature = ", feature), 
+           color = feature) +
+      xlab(group) + 
+      ylab(MDS1.lab)
+    
+    print(PCGroup_plot_sso)
+    
+    
+    # PLOT FOR PC AXES VIA BIPLOT #
+    # read in metadata %>% select only relevant info %>% remove redundant rows
+    meta.standalone <- read.csv(paste0('~/beta_diversity_testing/',study,'/meta.txt'), 
+                                sep = '\t', header = TRUE, check.names = FALSE) %>%
+      select(index, feature) %>% distinct()
+    row.names(meta.standalone) <- meta.standalone[[index]]
+    
+    data.biplot <- parse_ords(prop_exp_file) # df from biplot, NOT state sub ordination
+    
+    PC_plot_biplot <- ggplot(data.biplot, aes(x=PC1, y=PC2, color=meta.standalone[[feature]])) +
+      geom_point(size=2) + 
+      labs(title = paste(study, beta_method, ntaxa, "PCOA RESULTS"), 
+           subtitle = paste("meta feature = ", feature), 
+           color = feature) +
+      xlab(MDS1.lab) + 
+      ylab(MDS2.lab)
+    
+    print(PC_plot_biplot)
   }
 }
 dev.off()
