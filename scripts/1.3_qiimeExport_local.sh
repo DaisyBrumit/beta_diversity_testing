47c1786c348bf51319137864d3ee3126dda5335c
Daisy Fry Brumit <dfrybrum@Daisys-MacBook-Pro.local>
2024-08-07 12:22:51 -0400

    remove comment placeholders

--- scripts/1.3_qiimeExport_local.sh
+++ scripts/1.3_qiimeExport_local.sh
@@ -7,35 +7,35 @@ study=$1
 echo "Changing study to ~/beta_diversity_testing/${study}/qiime"
 cd ~/beta_diversity_testing/${study}/qiime
 
-#echo ""
-#echo "performing export from .qza to biom on ${study} filtered table"
-#if [ ${study} == "gemelli_ECAM" ]
-#then
-#	qiime tools export \
-#		--input-path table.qza \
-#		--output-path ../
-#else
-#	qiime tools export \
-#		--input-path filtered_table.qza \
-#		--output-path ../
-#fi
+echo ""
+echo "performing export from .qza to biom on ${study} filtered table"
+if [ ${study} == "gemelli_ECAM" ]
+then
+	qiime tools export \
+		--input-path table.qza \
+		--output-path ../
+else
+	qiime tools export \
+		--input-path filtered_table.qza \
+		--output-path ../
+fi
 
 # rename feature-table.biom to filtered_table.biom for continuity
-#mv ../feature-table.biom ../filtered_table.biom
+mv ../feature-table.biom ../filtered_table.biom
 
-#echo ""
-#echo "converting filtered_table.biom to tsv"
-#biom convert -i ../filtered_table.biom -o ../filtered_table.txt --to-tsv
+echo ""
+echo "converting filtered_table.biom to tsv"
+biom convert -i ../filtered_table.biom -o ../filtered_table.txt --to-tsv
 
-#echo ""
-#echo "exporting insertion tree to newick format"
-#qiime tools export \
-#	--input-path insertionTree.qza \
-#	--output-path ../insertionTreeDir
+echo ""
+echo "exporting insertion tree to newick format"
+qiime tools export \
+	--input-path insertionTree.qza \
+	--output-path ../insertionTreeDir
 
 # get tree file from qiime's exported tree directory
-#mv ../insertionTreeDir/tree.nwk ../insertion_tree.nwk
-#rm -r ../insertionTreeDir/
+mv ../insertionTreeDir/tree.nwk ../insertion_tree.nwk
+rm -r ../insertionTreeDir/
 
 echo ""
 echo "exporting rarefied table"
