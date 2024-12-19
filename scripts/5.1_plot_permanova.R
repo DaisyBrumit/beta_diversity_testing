d724e02753aafbea4db961aea573e311977947ac
Daisy Fry Brumit <dfrybrum@Daisys-MacBook-Pro.local>
2024-12-19 11:54:45 -0500

    explore with extreme outliers

--- scripts/5.1_plot_permanova.R
+++ scripts/5.1_plot_permanova.R
@@ -2,84 +2,100 @@
 
 rm(list=ls())
 library(tidyverse)
+source('~/beta_diversity_testing/scripts/functions/plotting_functions.R')
 
-#studyList <- c('Zeller', 'Vangay', 'Noguera-Julian')
-studyList <- c('Jones', 'ECAM')
-qiimeList <- c('phylo_rpca', 'rpca', 'ctf', 'phylo_ctf')
+studyList <- c('Zeller', 'Vangay', 'Noguera-Julian', 'Jones', 'Ruiz-Calderon')
+read.study.permanova <- function(path) {
 
-#plot_file <- '~/beta_diversity_testing/plots/psuedoF_plots.pdf'
-#pdf(plot_file)
+  tmp.table <- read.table(path, header = TRUE)
+  tmp.table$study <- str_replace(study, "_", " ")
+
+  # make a log transformed version of F stat
+  tmp.table$log_psuedoF = log10(tmp.table$psuedoF + 1)
+
+  # fill color by significance
+  tmp.table$signif <- case_when(tmp.table$pval < 0.05 ~ "**",
+                                tmp.table$pval < 0.1 ~ "*", TRUE ~ "")
+
+  return(tmp.table)
+}
+
+plot_file <- '~/beta_diversity_testing/plots/psuedoF_plots.pdf'
+pdf(plot_file)
 
 # create a whole table for summary info
 full.table <- tibble(beta = character(), psuedoF = numeric(), pval = numeric(), 
                       study = character(), log_psuedoF = numeric())
+full.shuffle <- tibble(beta = character(), psuedoF = numeric(), pval = numeric(),
+                      study = character(), log_psuedoF = numeric(), iteration=numeric())
 
 for (study in studyList) {
   
   setwd(paste0('~/beta_diversity_testing/',study))
   getwd() # sanity check
+
+  # study specific plot with "fill" by beta
+  study.table <- read.study.permanova(path = paste0('./permanova/permanova_results.tsv'))
+  study.table$beta <- str_replace(study.table$beta, "_", " ")
+  study.table <- study.table %>% filter(!beta %in% c('ctf', 'phylo ctf'))
   
-  study.table <- read.table(paste0('./permanova/permanova_results.tsv'), header = TRUE)
-  study.table$study <- str_replace(study, "_", " ")
-  
-  # make a log transformed version of F stat
-  study.table$log_psuedoF = log10(study.table$psuedoF + 1)
-    
+  study.plot <- f.plot.study(study.table, title = paste(study,"PsuedoF"))
+  print(study.plot)
+
   # append study table to full table
   full.table <- full.table %>% bind_rows(., study.table)
-  
-  # fill color by significance
-  study.table$signif <- case_when(study.table$pval < 0.05 ~ "**", 
-                                  study.table$pval < 0.1 ~ "*", TRUE ~ "")
-  
-  # study specific plot with "fill" by beta
-  studyplot <- ggplot(study.table, aes(x=beta, y=psuedoF)) +
-    geom_boxplot() +
-    geom_point() +
-    scale_color_brewer(palette="Dark2") +
-    geom_text(aes(label=signif),color = "red") +
-    labs(title = paste(study,"PsuedoF")) +
-    theme_minimal() +
-    theme(axis.text.x = element_text(angle = 30, vjust = 0.7))
-  
-  #log.studyplot <- ggplot(study.table, aes(x=beta, y=log_psuedoF)) +
-    #geom_boxplot() +
-    #labs(title = paste(study,"log10 PsuedoF"), 
-     #    subtitle = "Whole data vs HA taxa") +
-    #theme_minimal() +
-    #theme(axis.text.x = element_text(angle = 30, vjust = 0.7))
-  
-  print(studyplot)
-  #print(log.studyplot)
-}
 
-# meta plot (all studies) with and without fill (same as study plots above)
-fullplot <- ggplot(full.table, aes(x=beta, y=psuedoF)) +
+  # make table for shuffled results
+  #study.shuffle <- read.study.permanova(path = paste0('./permanova/permanova_results_shuffled.tsv'))
+  #study.shuffle$beta <- str_replace(study.shuffle$beta, "_", " ")
+  
+  #full.shuffle <- full.shuffle %>% bind_rows(., study.shuffle)
+}
+# set orders
+
+# meta plots (all studies)
+plot <- f.plot.multi(full.table, title = 'PERMANOVA F stats')
+print(plot)
+
+#specific add in: remove extreme outlier in Ruiz-Calderon
+study.table <- study.table %>% filter(psuedoF < 5000)
+full.table <- full.table %>% filter(psuedoF < 5000)
+
+dev.off()
+
+# shuffled permanova plots
+
+shuffle_plot_file <- '~/beta_diversity_testing/plots/psuedoF_plots_shuffled.pdf'
+#pdf(shuffle_plot_file)
+
+full.shuffle$log_pval <- log10(full.shuffle$pval + 0.000001)
+plt <- ggplot(full.shuffle, aes(x=beta, y=psuedoF)) +
   geom_boxplot() +
   geom_point(aes(color=study)) +
-  scale_color_brewer(palette="Dark2") +
-  #labs(title = paste("PsuedoF: All Studies")) +
+  scale_color_brewer(palette="Set1") +
   theme_minimal() +
-  theme(axis.text = element_text(size=14),
-        axis.title = element_text(size=14, face = 'bold'),
-        axis.text.x = element_text(angle = 30, vjust = 0.7),
-        legend.text = element_text(size=14),
-        legend.title = element_text(size=14, face = 'bold')) +
-  labs(x='Beta Metric', y='Pseudo-F Value', color='Study')
+  theme(axis.text = element_text(size=12),
+        axis.title = element_text(size=12, face = 'bold'),
+        axis.text.x = element_text(angle = 90, vjust = 0.7),
+        legend.title=element_blank(),
+        legend.text = element_blank()) +
+        #legend.text = element_text(size=12),
+        #legend.title = element_text(size=12, face = 'bold')) +
+  labs(x='Beta', y='Pseudo-F Value', color="Study")
+print(plt)
 
-#log.fullplot <- ggplot(full.table, aes(x=beta, y=log_psuedoF)) +
-  #geom_boxplot() +
-  #geom_point(aes(color=study)) +
-  #labs(title = paste("log10 PsuedoF: All Studies"), 
-       #subtitle = "Whole data vs HA taxa") +
-  #theme_minimal() +
-  #theme(axis.text.x = element_text(angle = 30, vjust = 0.7),
-        #legend.title = element_blank(),
-        #axis.title.x = element_blank())
+# unique beta values
+beta_values <- unique(full.shuffle$beta)
 
-print(fullplot)
-#print(log.fullplot)
-
+# plot for each beta value
+for (beta_value in beta_values) {
+  x <- full.shuffle %>% filter(beta == beta_value)
+  
+  plt <- ggplot(x, aes(x = psuedoF)) +
+    geom_histogram() +
+    labs(title = beta_value)
+  print(plt)
+}
 
 #dev.off()
 
